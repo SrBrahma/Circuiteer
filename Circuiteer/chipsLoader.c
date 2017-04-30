7460c219592328af98c8f992f7650f1f07dc615f
Unknown <henrique271197@gmail.com>
2017-04-30 02:49:04 -0300

    0.0.2

--- Circuiteer/chipsLoader.c
+++ Circuiteer/chipsLoader.c
@@ -1,23 +1,45 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "Circuiteer.h"
 
 #define ERROR_OPENING_FILE					1
 #define ERROR_STR_TO_UL						2
 
-#define STRING_SIZE					128
-#define COMMENTARY_START			'#'
+#define COMMENTARY_START					'#'
 
-/*will load the chips into the given array*/
+/* Will load the array with the chars, and will return the amount of chars, including the EOS.
+ * EX: "abc\0" will return 4 chars */
+ 
+unsigned
+readTextFileLine (FILE *pFile, char arrayToReturn[])
+{
+	arrayToReturn[0] = '0';
+	unsigned charCounter = 0;
+	do
+	{
+		arrayToReturn[charCounter] = fgetc(pFile);
+		charCounter ++;
+	}
+	while (arrayToReturn[charCounter-1] != '\n' && arrayToReturn[charCounter-1] != EOF && arrayToReturn[charCounter-1] != EOS);
+	
+	arrayToReturn[charCounter-1] = EOS;
+	return charCounter;
+}
 
+/* Will load the chips into the given array */
 unsigned short
 LoadChips (ChipType chipsArrayToReturnToProgram[NUMBER_OF_GATES_KINDS][MAX_NUMBER_OF_INPUTS_PER_OUTPUT][MAX_NUMBER_OF_OUTPUTS][MAX_NUMBER_CHIPS_PER_GATES])
 {
-	unsigned short counterChipModels[NUMBER_OF_GATES_KINDS][MAX_NUMBER_OF_INPUTS_PER_OUTPUT][MAX_NUMBER_OF_OUTPUTS];
+	unsigned short counterChipModels [NUMBER_OF_GATES_KINDS][MAX_NUMBER_OF_INPUTS_PER_OUTPUT][MAX_NUMBER_OF_OUTPUTS];
 	ChipType auxChipVar;
-	char myString [STRING_SIZE];
+	char *stringValidation;
+	char myString [MAX_CHIP_DESCRIPTION_SIZE];
 	int short controlString = 0;
+	unsigned charCounter;
 
-
+	myString[0] = 0;
+	
 	FILE *pFile;
 	
 
@@ -25,60 +47,71 @@ LoadChips (ChipType chipsArrayToReturnToProgram[NUMBER_OF_GATES_KINDS][MAX_NUMBE
 	
 	if (pFile == NULL) return ERROR_OPENING_FILE;
 	
-	while (fgets (myString, STRING_SIZE, pFile) != NULL)
+	while (feof (pFile) == 0)
 	{
+		charCounter = readTextFileLine (pFile, myString);
+		printf ("RAW: %s, control = %i\n", myString, controlString);
 		if (controlString == 0) /* Check if the line is a commentary or empty line */
 		{
-			while (myString[0] == COMMENTARY_START || myString[0] == EOS) /* Read the file until commentary/empty line ends */
+
+			if (myString[0] == EOS || myString[0] == COMMENTARY_START)
 			{
-				fgets (myString, STRING_SIZE, pFile);
+				controlString = -1;
 			}
-			controlString = 1;
+			else
+			{
+				controlString = 1;
+			}
 		}
 		
 		if (controlString == 1) /* Chip Name */
 		{
-			memcpy(auxChipVar.name, myString, strlen(STRING_SIZE
-			printf ("%s\n", myString);
+			memcpy(auxChipVar.name, myString, charCounter);
+			printf ("READ: %s\n", myString);
+			
 		}
 		
 		else if (controlString == 2) /* Chip Description */
 		{
-			auxChipVar.description = myString;
-			printf ("%s\n", myString);
+			memcpy(auxChipVar.description, myString, charCounter);
 		}
 		
 		else if (controlString == 3) /* Chip kind */
 		{
-			auxChipVar.gateKind = (unsigned short) strtoul (myString, *stringValidation, 10);
-			printf ("%s\n", myString);
+			auxChipVar.gateKind = (unsigned short) strtoul (myString, &stringValidation, 10);
 		}
 		
 		else if (controlString == 4) /* Chip number of inputs */
 		{
-			auxChipVar.inputs = (unsigned short) strtoul (myString, *stringValidation, 10);
-			printf ("%s\n", myString);
+			auxChipVar.inputs = (unsigned short) strtoul (myString, &stringValidation, 10);
 		}
 		
 		else if (controlString == 5) /* Chip number of outputs */
 		{
-			auxChipVar.outputs = (unsigned short) strtoul (myString, *stringValidation, 10);
-			printf ("%s\n", myString);
+			auxChipVar.outputs = (unsigned short) strtoul (myString, &stringValidation, 10);
 		}
 		
 		else if (controlString == 6) /* Chip ratio of inputs per output */
 		{
-			auxChipVar.inputsPerOutput = (unsigned short) strtoul (myString, *stringValidation, 10);
-			printf ("%s\n", myString);
-			controlString = 0; /*restart the counter/control */
+			auxChipVar.inputsPerOutput = (unsigned short) strtoul (myString, &stringValidation, 10);
+			controlString = 0; /* Restart the counter/control */
+			break;
 		}
-	
-		controlString += 1;
 		
-		if (stringValidation != EOS) {
+		
+		if (controlString > 2 && *stringValidation != EOS)
+		{
 			return ERROR_STR_TO_UL;
 		}
-	} /* END OF WHILE LOOP */
+		
+		controlString += 1;
+	} 									/* END OF WHILE LOOP */
+	
+	printf ("Name: %s\n", auxChipVar.name);
+	/*printf ("Description: %s\n", auxChipVar.description);
+	printf ("Gate: %u\n", auxChipVar.gateKind);
+	printf ("Inputs: %u\n", auxChipVar.inputs);
+	*/
 	fclose (pFile);
 	return 0;
 }
