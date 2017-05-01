488f7b78ea51ebeae0095c581396b04c83df4c59
Henrique Bruno <henrique271197@gmail.com>
2017-04-30 21:22:52 -0300

    0.0.6
    Few updates from previous version.

--- Circuiteer/Circuiteer.c
+++ Circuiteer/Circuiteer.c
@@ -14,7 +14,7 @@ int
 main (int argc, char *argv[])
 {
 	ChipType chipsArray[NUMBER_OF_GATES_KINDS][MAX_NUMBER_OF_INPUTS_PER_OUTPUT][MAX_NUMBER_OF_OUTPUTS][MAX_NUMBER_CHIPS_PER_GATES];
-	char agroupments[64];
+	char agroupments[MAX_OUTPUTS][32];
 	unsigned short inputs, outputs, numberAgroupments, uSCounter;
 	unsigned short validationLoadChips;
 	char inputsNames[MAX_INPUTS][INPUTS_OUTPUTS_MAX_LENGHT_NAME];
@@ -22,19 +22,28 @@ main (int argc, char *argv[])
 	char auxString[128];
 	
 	validationLoadChips = LoadChips (chipsArray);
-	printf("LoadChip Exit Code: %u\n", validationLoadChips);
+	printf("LoadChip Exit Code: %u\n\n", validationLoadChips);
 	
 	sprintf (auxString, "Enter the number of inputs ([1, %u]):\n", MAX_INPUTS);
 	inputs = scanfUnsigned(auxString, 1, MAX_INPUTS, MAX_ENTRY_LENGHT);
+	
 	printf("\n");
 	for (uSCounter = 0; uSCounter < inputs; uSCounter ++)
 	{
 		sprintf (auxString, "Enter the corresponding letter to the input number %u:\n", uSCounter);
 		inputsNames[uSCounter][0] = scanfChar (auxString, 0, MAX_ENTRY_LENGHT);
 	}
+	
 	printf("\n");
 	sprintf (auxString, "Enter the number of outputs ([1, %u]):\n", MAX_OUTPUTS);
 	outputs = scanfUnsigned(auxString, 1, MAX_OUTPUTS, MAX_ENTRY_LENGHT);
+	
+	printf("\n");
+	for (uSCounter = 0; uSCounter < outputs; uSCounter ++)
+	{
+		sprintf (auxString, "Enter the corresponding letter to the output number %u:\n", uSCounter);
+		outputsNames[uSCounter][0] = scanfChar (auxString, 0, MAX_ENTRY_LENGHT);
+	}
 	return OK;
 }
 
